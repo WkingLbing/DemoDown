17b937cadbd74283bf37b02e84d7bfabe1c63924
Lbing <1923669918@qq.com>
2022-05-18 14:47:21 +0800

    初始化

---
+++ src/main/java/com/example/demodown/DemoDownApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDownApplication.class, args);
+    }
+
+}
